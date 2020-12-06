173a77357b297a7e996f738c6a1d5443fccd8eac
kellysjung <kelly.jung@lessannoyingcrm.com>
2020-12-06 14:56:08 -0500

    day7 setup'

---
+++ 2020/day_07/solution1.py
@@ -0,0 +1,7 @@
+def open_input(day):
+    with open('./2020/day_{}/input.txt'.format(day), 'r') as file:
+        data = file.read()
+        split_data = data.splitlines()
+        return split_data
+
+input = open_input("07")
